0c2fff4e9bcfefa3a3613ecdb976ec5d86a93a55
prabhath-kj <view.prabhath.kj@gmail.com>
2023-08-02 16:13:23 +0530

    Ready to deploy

--- src/services/mailServices.js
+++ src/services/mailServices.js
@@ -1,22 +1,22 @@
 import {createTransport} from "nodemailer";
-
-
+import * as dotenv from "dotenv"
+dotenv.config()
 
 const sendMail = async (email, subject, text) => {
   try {
     const transporter = createTransport({
-      host: "smtp.gmail.com",
-      service:"gmail",
+      host:process.env.HOST,
+      service:process.env.SERVICE,
       secure: false,
       auth: {
-        user: "prabhathkj4@gmail.com",
-        pass: "qexxvcbwwkcelywt",
+        user: process.env.USER,
+        pass: process.env.PASS,
       },
     });
 
     // Send the email using the transporter
     await transporter.sendMail({
-      from: "prabhathkj4@gmail.com",
+      from: process.env.USER,
       to: email,
       subject: subject,
       text: text,
